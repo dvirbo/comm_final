6f0125ff39810117de112d09c739efdeec4866fc
dvirbo <dvir.borochov@gmail.com>
2022-07-27 18:34:44 +0300

    tcp part a finish

--- net_client.c
+++ net_client.c
@@ -7,42 +7,57 @@
 #include <signal.h>
 #include <sys/socket.h>
 #include <sys/un.h>
+#include <netdb.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
 #define SIM_LENGTH 10
 #define IP_ADDRESS "127.0.0.1"
 #define PORT 1337
-int main(void)
+
+int main(int argc, char *argv[])
 {
-  int sock;
-  struct sockaddr_in cli_name;
-  int count;
-  int value;
+  int sock, count, value;
+  struct sockaddr_in cli_name, *saddr;
+  struct addrinfo *res;
+  char *hostname, *hostaddr;
 
   printf("Client is alive and establishing socket connection.\n");
-
-  sock = socket(AF_INET, SOCK_STREAM, 0);
-  if (sock < 0)
+  if (argc != 2)
+  {
+    perror("Usage: hostnamelookup <hostname>\n");
+    exit(1);
+  }
+  hostname = argv[1]; // get hostname as a command-line argument
+  if (0 != getaddrinfo(hostname, NULL, NULL, &res)) //// speak with local DNS
+  {
+    fprintf(stderr, "Error in resolving hostname %s\n", hostname);
+    exit(1);
+  }
+  saddr = (struct sockaddr_in *)res->ai_addr;
+  hostaddr = inet_ntoa(saddr->sin_addr); //Convert adress number to ASCII representation
+  printf("The IP for hostname %s is %s\n", hostname, hostaddr);
+  sock = socket(AF_INET, SOCK_STREAM, 0); // Create a new socket
+  if (sock < 0) // check if -1 for errors
   {
     perror("Error opening channel");
     close(sock);
     exit(1);
   }
 
-  bzero(&cli_name, sizeof(cli_name));
+  bzero(&cli_name, sizeof(cli_name));  
   cli_name.sin_family = AF_INET;
-  cli_name.sin_addr.s_addr = inet_addr(IP_ADDRESS);
-  cli_name.sin_port = htons(PORT);
+  cli_name.sin_addr.s_addr = inet_addr(hostaddr);
+  cli_name.sin_port = htons(PORT); 
 
-  if (connect(sock, (struct sockaddr *)&cli_name, sizeof(cli_name)) < 0)
+  if (connect(sock, (struct sockaddr *)&cli_name, sizeof(cli_name)) < 0) 
   {
     perror("Error establishing communications");
     close(sock);
     exit(1);
   }
 
-  for (count = 1; count <= SIM_LENGTH; count++)
+  for (count = 1; count <= SIM_LENGTH; count++) 
   {
     read(sock, &value, 4);
     printf("Client has received %d from socket.\n", value);
